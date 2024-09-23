6c57651ded3064fd5e21393e0bcebcd9bbe0a038
daniel <danielstmontoya@gmail.com>
2024-09-23 10:54:57 -0500

    API created with methods GET and POST completed

---
+++ functions.py
@@ -0,0 +1,16 @@
+def get_book_by_id(id, books: list):
+  """
+  Retrieves a book by its ID from a list of books.
+
+  Args:
+    id: The ID of the book to retrieve. If 'all', returns the entire list of books.
+    books (list): A list of dictionaries representing books, each with a 'book_id' key.
+
+  Returns:
+    A dictionary representing the book with the matching ID, or the entire list of books if ID is 'all'.
+  """
+  if id == 'all':
+    return books
+  for book in books:
+    if book['book_id'] == id:
+      return book
--- main.py
+++ main.py
@@ -1,14 +1,44 @@
 from typing import Optional
+import functions as fn
 
 from fastapi import FastAPI
 
 app = FastAPI()
 
+BOOKS = [
+    {'book_id':1,'title': 'Title 1', 'author': 'Author 1', 'category': 'science'},
+    {'book_id':2,'title': 'Title 2', 'author': 'Author 2', 'category': 'science'},
+    {'book_id':3,'title': 'Title 3', 'author': 'Author 3', 'category': 'history'},
+    {'book_id':4,'title': 'Title 4', 'author': 'Author 4', 'category': 'math'},
+    {'book_id':5,'title': 'Title 5', 'author': 'Author 5', 'category': 'math'},
+    {'book_id':6,'title': 'Title 6', 'author': 'Author 6', 'category': 'math'}
+]
 
-@app.get("/")
+
+@app.get("/home")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "This is a basic API I created to simulate a real API with the 4 main verbs: GET, POST, PUT and DELETE"}
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/get/{book_id}")
+def get_book(book_id):
+    """
+    Retrieves a book by its ID.
+
+    Args:
+        book_id: The ID of the book to retrieve.
+
+    Returns:
+        A dictionary representing the book with the matching ID.
+    """
+    return fn.get_book_by_id(book_id, BOOKS)
+
+@app.post("/post/{book}")
+def post_book(book: str):
+    arr = book.split(',')
+    BOOKS.append({
+        'book_id':int(arr[0].strip()),
+        'title':arr[1].strip(),
+        'author':arr[2].strip(),
+        'category':arr[3].strip(),
+    })
+    return "Book added"
